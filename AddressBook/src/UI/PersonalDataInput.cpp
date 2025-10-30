83b743668c91db060d4fae7f76f5b71fa4c7bdf3
JIINY <eastrin@gmail.com>
2025-10-31 01:29:54 +0900

    저장시 누락된 것 재반영

--- AddressBook/src/UI/PersonalDataInput.cpp
+++ AddressBook/src/UI/PersonalDataInput.cpp
@@ -1,6 +1,7 @@
 #include "PersonalDataInput.hpp"
 #include <memory>
 #include <optional>
+#include <cassert>
 
 #include "States/InputNameState.hpp"
 #include "States/InputPhoneState.hpp"
@@ -12,6 +13,7 @@ using namespace std;
 
 PersonalData PersonalDataInput::run(optional<PersonalData> initialData) 
 {
+	//멤버변수 초기화
 	if (initialData.has_value()) 
 	{
 		data_ = initialData.value();
@@ -19,7 +21,70 @@ PersonalData PersonalDataInput::run(optional<PersonalData> initialData)
 	else 
 	{
 		data_ = PersonalData{};
-	}
-	
+	}	
 	lastError_ = nullopt;
+
+	//시작 상태로 설정(이름 입력)
+	DataInputPhase currentPhase = DataInputPhase::InputName;
+	transitionTo(currentPhase);
+
+	while (currentPhase != DataInputPhase::Exit) 
+	{
+		currentState_->draw();
+		
+		DataInputPhase nextPhase = currentState_->update();
+		if (nextPhase != currentPhase) 
+		{
+			transitionTo(nextPhase);
+			currentPhase = nextPhase;
+		}
+	}
+
+	return data_;
+}
+
+void PersonalDataInput::transitionTo(DataInputPhase nextPhase) 
+{
+	if (nextPhase != DataInputPhase::Exit) 
+	{
+		ui_.clearScreen();
+	}
+
+	switch (nextPhase) {
+	case DataInputPhase::InputName:
+	{
+		currentState_ = make_unique<InputNameState>(*this);
+		break;
+	}
+	case DataInputPhase::InputPhone:
+	{
+		currentState_ = make_unique<InputPhoneState>(*this);
+		break;
+	}
+	case DataInputPhase::InputAddress:
+	{
+		currentState_ = make_unique<InputAddressState>(*this);
+		break;
+	}
+	case DataInputPhase::InputZipCode:
+	{
+		currentState_ = make_unique<InputZipCodeState>(*this);
+		break;
+	}
+	case DataInputPhase::InputEmail:
+	{
+		currentState_ = make_unique<InputEmailState>(*this);
+		break;
+	}
+	case DataInputPhase::Exit:
+	{
+		currentState_ = nullptr;
+		break;
+	}
+	default: 
+	{
+		assert(false && "PersonalDataInpu::transitionTo: unhandled DataInputPhase.");
+		break;
+	}
+	}
 }
